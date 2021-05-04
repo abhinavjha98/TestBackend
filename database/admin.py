ae877d303cbc549c32788ae827c6da12a3930b1f
Abhinav Jha <abhinavjha98ald@gmail.com>
2021-05-04 18:25:11 +0530

    New app

---
+++ database/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from database.models import *
+from import_export.admin import ImportExportModelAdmin
+# Register your models here.
+
+class aa419Admin(ImportExportModelAdmin):
+    list_display = ('url', 'label')
+    search_fields = ('url', 'label')
+    list_filter = ('url', 'label')
+
+admin.site.register(aa419)
---
+++ database/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, )
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class aa419(Base):
+    url = models.CharField(max_length=200, null=True)
+    label = models.CharField(max_length=50,null=True)
